09aa9cb65ee69b3602cdcf099d83a3c483655572
mingkaijiang <shanyujordan512@gmail.com>
2017-06-14 12:36:11 +1000

    finished preliminary gap fill, ignoring all problematic sites

--- Run_programs.R
+++ Run_programs.R
@@ -60,11 +60,12 @@ Missing_check(sourceDir = "data/ghcnd_selected")
 ### Gap filling 1. - use statistical correlation among 9 stations to gap fill one station
 ###                - excluding big chunk of missing data
 ###                - Problematic sites: row 12, 14, 15, 16, 17, 18, 25,26,29,30,34,35,36,37, 40,41
-###                - 42, 46, 47, 48, 50,51,52,53,62,63,64,
+###                - 42, 46, 47, 48, 50,51,52,53,62,63,64,66,67,68,69,70,
+###                - 74, 77, 78, 
 
 stationDF1 <- stationDF[-c(1:12),]
 
-stationDF1 <- stationDF[-c(1:64),]
+stationDF1 <- stationDF[-c(1:78),]
 
 Gap_Fill(stationDF1, sourceDir = "data/ghcnd_selected", destDir = "data/ghcnd_gap_filled")
 
