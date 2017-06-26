c5067914f48a4537a02e412157370b775a8db613
mingkaijiang <shanyujordan512@gmail.com>
2017-06-26 22:18:11 +1000

    add prepare R file

---
+++ R/prepare_R.R
@@ -0,0 +1,10 @@
+#### prepare R scripts
+
+#### all functions
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(reshape, reshape2, lubridate, eeptools, hyfo) # add other packages needed to this list
+
+
+# Sourcing all R files in the modules subdirectory
+sourcefiles <- dir("modules", pattern="[.]R$", recursive = TRUE, full.names = TRUE)
+for(z in sourcefiles)source(z)
