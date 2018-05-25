95ffc75523f888c81e840cc98b61621c049b0eec
bfelzer <bsf208@lehigh.edu>
2018-05-25 19:01:46 -0400

    Fixed error in destination directory

--- modules/ThrIndS_tmax_ann.R
+++ modules/ThrIndS_tmax_ann.R
@@ -2,7 +2,7 @@
 ThrIndS_tmax_ann <- function(sourceDir = DAILY.DATA.DIRECTORY, destDir = DAILY.OUTPUT.DIRECTORY) 
 
 {
-  sourceDir = "data/ghcnd_gap_filled"
+  sourceDir = "data/ghcnd_tmax_gap_filled_tmax"
   destDir = "data/indices/ThrIndS_tmax"
   dir.create(destDir, showWarnings = FALSE)
   DatFiles <- list.files(path = sourceDir, pattern = "\\.csv")
